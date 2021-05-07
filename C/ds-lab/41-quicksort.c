be3995b7fad851558a4494375c2c9232b8209e8d
zshashz <zshashwatz@gmail.com>
2021-05-07 18:21:16 +0530

    Create 41-quicksort.c

---
+++ C/ds-lab/41-quicksort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+int partition(arr, int low, int high);
+
+void QuickSort(arr,n);
+
+int main()
+{
+    int arr[100], n, i;
+    
+    printf("Enter number of elements \n");
+    scanf("%d", &n);
+    
+    printf("Enter %d Numbers \n", n);
+    
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    QuickSort(arr,n);
+    
+    return 0;
+
+}
+
+void swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(arr, low, high)
+{
+    // select rightmost element as pivot
+    int pivot = arr[high]
+
+}
+
+
+void QuickSort(arr,n)
+{
+
+}
