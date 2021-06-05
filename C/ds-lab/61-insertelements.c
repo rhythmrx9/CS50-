1b9a848d8692993476333305349bfe4730ca4ad3
zshashz <zshashwatz@gmail.com>
2021-06-05 19:59:09 +0530

    Create 61-insertelements.c

---
+++ C/ds-lab/61-insertelements.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node 
+{
+  int element;
+  struct node *next;
+};
+
+void insert()
+{
+    struct node* temp;
+    temp = (struct node*) malloc(sizeof(struct node));
+
+    printf("enter the data");
+    scanf("%d", &temp->element);
+
+    temp->next=NULL;
+
+    
+}
+
