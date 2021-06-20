7504c0915e77e6d27871723a46f20469e089410e
zshashz <zshashwatz@gmail.com>
2021-06-20 19:07:05 +0530

    Update 24-bubblesortlist.c

--- C/ds-lab/24-bubblesortlist.c
+++ C/ds-lab/24-bubblesortlist.c
@@ -1,4 +1,4 @@
-// Lab-2 Program 4: Write a C Program to Convert unordered list into ordered one Using Bubble Sort
+// Lab 2 Program 4 : Write a C Program to Convert unordered list into ordered one Using Bubble Sort
 
 #include <stdio.h>
 
