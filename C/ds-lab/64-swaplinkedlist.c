b07c01c1a4a5b571228993bb418eee404c7edc80
zshashz <72076441+zshashz@users.noreply.github.com>
2021-07-05 17:50:56 +0530

    Update 64-swaplinkedlist.c

--- C/ds-lab/64-swaplinkedlist.c
+++ C/ds-lab/64-swaplinkedlist.c
@@ -3,8 +3,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node 
+struct node
 {
-  int element;
-  struct node *next;
+int data;
+struct node *link;
 };
+
+int main()
+{
+    struct node *head=(struct node*)malloc(sizeof(struct node));
+  
+    head->data=15;
+    head->link=NULL;
+  
+    struct node *current=(struct node*)malloc(sizeof(struct node));
+    current->data=18;
+    head->link=current;
+    current->link=NULL;
+  
+    current=(struct node*)malloc(sizeof(struct node));
+    current->data=21;
+    current->link=NULL;
+    head->link->link=current;
+  
+    printf("original list:\n");
+    while(current!=NULL)
+    {
+        printf("%d\t",current->data);
+        current=current->link;
+    }
+  
+    printf("list after swapping the first and last elements");
+   
+    int temp=current->data;
+  
+    current->data=head->data;
+    head->data=temp;
+    current=head;
+    printf("\n");
+    
+    while(current!=NULL)
+    {
+        printf("%d\t",current->data);
+        current=current->link;
+    }
+  
+    return 0;
+}
