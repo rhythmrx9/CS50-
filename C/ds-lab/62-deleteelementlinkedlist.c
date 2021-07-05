93025284d0e0f77f3519d2473a23d2128c6a85f0
zshashz <72076441+zshashz@users.noreply.github.com>
2021-07-05 17:37:35 +0530

    Update 62-deleteelementlinkedlist.c

--- C/ds-lab/62-deleteelementlinkedlist.c
+++ C/ds-lab/62-deleteelementlinkedlist.c
@@ -3,13 +3,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node 
+void delete();
+struct node
 {
-  int element;
-  struct node *next;
+int data;
+struct node *link;
 };
-
 int main()
 {
+struct node *head=(struct node*)malloc(sizeof(struct node));
+head->data=45;
+head->link=NULL;
   
+struct node *current=(struct node*)malloc(sizeof(struct node));
+current->data=98;
+head->link=current;
+current->link=NULL;
+current=(struct node*)malloc(sizeof(struct node));
+current->data=23;
+current->link=NULL;
+head->link->link=current;
+
+int loc;
+printf("enter which element u wanna delete : ");
+scanf("%d",&loc);
+delete(&head,loc);
+
+printf("Now the element in list are : \n");
+current=head;
+while(current!=NULL)
+{
+  printf("%d ",current->data);
+  current=current->link;
+}
+return 0;
+}
+
+void delete(struct node **head, int loc)
+{
+//node to be wanna delete
+struct node *current=*head;
+// previous node
+struct node *previous=*head; 
+if (*head==NULL)
+{
+  printf("List is already empty");
+}
+else if(loc==1)
+{
+  printf("%d is deleted \n",current->data);
+  //move head
+  *head=current->link;
+  free(current);
+  current=NULL;
+}
+else
+{
+while(loc!=1)
+{
+  previous=current;
+  current=current->link;
+  loc--;
+}
+ 
+printf("%d is deleted \n",current->data);
+previous->link=current->link;
+free(current);
+current=NULL;
+}
 }
