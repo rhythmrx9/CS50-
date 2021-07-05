c86e25cabd4b43c567202874c1457de705c3c35c
zshashz <72076441+zshashz@users.noreply.github.com>
2021-07-05 17:43:38 +0530

    Update 63-traverseelementslinkedlist.c

--- C/ds-lab/63-traverseelementslinkedlist.c
+++ C/ds-lab/63-traverseelementslinkedlist.c
@@ -3,13 +3,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node 
+struct node
 {
-  int element;
-  struct node *next;
+int data;
+struct node *link;
 };
 
 int main()
 {
+    struct node *head=(struct node*)malloc(sizeof(struct node));
   
+    head->data=15;
+    head->link=NULL;
+  
+    struct node *current=(struct node*)malloc(sizeof(struct node));
+    current->data=18;
+    head->link=current;
+    current->link=NULL;
+    current=(struct node*)malloc(sizeof(struct node));
+    current->data=21;
+    current->link=NULL;
+  
+    head->link->link=current;
+    current=head;
+  
+    printf("the elements in the list are :\n");
+    while(current!=NULL)
+    {
+        printf("%d\t",current->data);
+        current=current->link;
+    }
+    return 0;
 }
